ffb3b9f24f5cdaf32ea75cd53662c9ae0f228ba9
Alex Buznik <shu@buznik.net>
2020-06-01 23:14:53 +0300

    Added state machine

--- Arduino/wifi-server/wifi-server.ino
+++ Arduino/wifi-server/wifi-server.ino
@@ -14,23 +14,67 @@
 #define SENSOR_ID "12345"
 #endif
 
-int sensor_ip[4] = {0, 0, 0, 0};
-bool sensor_registered = false;
+const int VERSION = 1;
+// hw
+const int RELAY_PIN = D4;
 
-const int SENSOR_THRESHOLD = 20;
-int last_measurement = null;
+// software
+HTTPClient http;
 
-// HTTPClient http;
+String sensor_host = "";
+
+// thresholds in cm; sensor value
+const int SENSOR_FULL_THRESHOLD = 20;
+const int SENSOR_LOW_THRESHOLD = 160;
+int last_measurement = -1;
+
+// default check is once per 15 minutes
+const int SENSOR_POLL_DELAY = 900000;
+// while pump is active check every second
+const int ACTIVE_SENSOR_POLL_DELAY = 1000;
 
 /* Set these to your desired credentials. */
 const char *ssid = APSSID;
 const char *password = APPSK;
 
+int SERVER_STATE = 0;
+const int STATE_IDLE = 0;
+const int STATE_ERROR = 1;
+const int STATE_SENSOR_CONNECTED = 2;
+const int STATE_SENSOR_OK = 3;
+const int STATE_SENSOR_LOW = 4;
+const int STATE_RELAY_CLOSED = 5;
+
+int get_state() {
+  return SERVER_STATE;
+}
+
 ESP8266WebServer server(80);
 
-/* Just a little test message.  Go to http://192.168.4.1 in a web browser
-   connected to this access point to see it.
-*/
+void state_on_registered() {
+  Serial.println("state_on_registered");
+  SERVER_STATE = STATE_SENSOR_CONNECTED;
+}
+void state_on_sensor_full() {
+  Serial.println("state_on_sensor_full");
+  SERVER_STATE = STATE_SENSOR_OK;
+}
+
+void state_on_sensor_low() {
+  Serial.println("state_on_sensor_low");
+  SERVER_STATE = STATE_SENSOR_LOW;
+}
+
+void state_on_relay_close() {
+  Serial.println("state_on_relay_close");
+  SERVER_STATE = STATE_RELAY_CLOSED;
+}
+
+void state_on_error() {
+  Serial.println("state_on_error");
+  SERVER_STATE = STATE_ERROR;
+}
+
 void handleRoot() {
   server.send(200, "text/html", "<h1>You are connected</h1>");
 }
@@ -39,27 +83,24 @@ void handleRegister() {
   if (server.method() != HTTP_POST) {
   server.send(405, "text/plain", "Method Not Allowed");
   } else {
-    String message = "POST form was:\n";
+    String message = "";
 
     if (server.arg("sensor_code") == SENSOR_ID) {
-      sensor_registered = true;
-      if (server.hasArg("sensor_ip")) {
-//        sensor_ip = server.arg("sensor_ip");
+      Serial.println("sensor_registered true");
+      if (server.hasArg("sensor_host")) {
+        sensor_host = server.arg("sensor_host");
+        state_on_registered();
+        Serial.println("Sensor registered @ " + sensor_host);
       }
     }
-
-    for (uint8_t i = 0; i < server.args(); i++) {
-      message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-      if (server.argName(i) == "sensor_code" && server.arg(i) == SENSOR_ID) {
-//        sensor_registered = true;
-      }
-    }
-
+    // debug
     Serial.println("handleRegister args:");
+    Serial.println(server.arg("sensor_code"));
+    Serial.println(SENSOR_ID);
+    Serial.println(server.arg("sensor_host"));
     Serial.println(message);
-    Serial.println(sensor_registered);
 
-    if (sensor_registered == true) {
+    if (get_state() == STATE_SENSOR_CONNECTED) {
       server.send(200, "text/html", "Registered");
     } else {
       server.send(401, "text/html", "Wrong arguments");
@@ -68,13 +109,56 @@ void handleRegister() {
   }
 }
 
+void readSensorData() {
+  HTTPClient http;
+  String URL = sensor_host + "/sensor";
+  http.begin(URL);
+  
+  Serial.println("readSensorData @ " + URL);
+ 
+  int httpCode = http.GET();
+  if (httpCode > 0) {
+    String payload = http.getString();
+    Serial.println("readSensorData GET: " + payload);
+    check_measurement(payload.toInt());
+  } else {
+    state_on_error();
+  }
+ 
+  http.end();
+  // #TODO replace with proper non-blocking delay
+  delay(ACTIVE_SENSOR_POLL_DELAY);
+}
 
+void check_measurement(int tmp_last_measurement) {
+  if (tmp_last_measurement >= 0) {
+    last_measurement = tmp_last_measurement;
+    // normal operation
+    if (get_state() == STATE_RELAY_CLOSED) {
+      // relay closed, waiting till the tank reaches FULL
+      if (last_measurement <= SENSOR_FULL_THRESHOLD) {
+        state_on_sensor_full();
+      }
+    } else {
+      // relay open, just watching the tank
+      if (last_measurement >= SENSOR_LOW_THRESHOLD) {
+        state_on_sensor_low();
+      }
+    }
+    
+  } else {
+    state_on_error();
+  }
+}
 
 void setup() {
-  delay(1000);
   Serial.begin(115200);
+
+  pinMode(RELAY_PIN, OUTPUT);
+  
   Serial.println();
   Serial.print("Configuring access point...");
+
   /* You can remove the password parameter if you want the AP to be open. */
   WiFi.softAP(ssid, password);
 
@@ -91,4 +175,26 @@ void setup() {
 
 void loop() {
   server.handleClient();
+
+  
+/*
+ * State Loop
+ * 
+ * If registered
+   * If relay open then poll sensor with timer REGULAR
+   * If SENSOR_LOW then close relay
+     * If SENSOR_OK then open relay
+     * poll sensor with timer ACTIVE
+ * 
+ */
+  if (get_state() >= STATE_SENSOR_CONNECTED) {    
+    readSensorData();
+
+    if (get_state() == STATE_SENSOR_LOW) {
+      state_on_relay_close();
+    }
+  }
+
+  digitalWrite(RELAY_PIN, get_state() == STATE_RELAY_CLOSED);
+
 }
